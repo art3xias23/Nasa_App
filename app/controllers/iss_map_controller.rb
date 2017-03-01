ceb5a74679dc0d206d2041255961ae8f64e6cbdc
art3xias23 <k.milchev.12@aberdeen.ac.uk>
2017-03-01 17:15:49 +0000

    started iss map

--- app/controllers/iss_map_controller.rb
+++ app/controllers/iss_map_controller.rb
@@ -1,5 +1,10 @@
 class IssMapController < ApplicationController
+  require 'net/http'
+
   def index
   	@iss_api = Iss.get_iss_api
+  	@iss_map = Iss.get_iss_map
   end
+
+ 
 end
--- app/models/iss.rb
+++ app/models/iss.rb
@@ -4,4 +4,10 @@ class Iss < ApplicationRecord
 		request_iss_api = Net::HTTP.get(URI(iss_api))
 		JSON.parse request_iss_api
 	end 
+
+	def self.get_iss_map
+		iss_map_api = "https://api.wheretheiss.at/v1/coordinates/37.795517,-122.393693"
+		request_iss_map_api = Net::HTTP.get(URI(iss_map_api))
+		JSON.parse request_iss_map_api
+	end 
 end
