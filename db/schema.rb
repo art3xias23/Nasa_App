be451cd170c880c4b3910f3f01d67144d307149e
Alex Donchev <a.donchev.12@aberdeen.ac.uk>
2017-03-03 19:17:03 +0000

    ready

--- db/schema.rb
+++ db/schema.rb
@@ -9,19 +9,6 @@
 # you'll amass, the slower it'll run and the greater likelihood for issues).
 #
 # It's strongly recommended that you check this file into your version control system.
-
-ActiveRecord::Schema.define(version: 20170303161306) do
-
-  create_table "microposts", force: :cascade do |t|
-    t.text     "content"
-    t.integer  "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["user_id", "created_at"], name: "index_microposts_on_user_id_and_created_at"
-    t.index ["user_id"], name: "index_microposts_on_user_id"
-  end
-end
-
 ActiveRecord::Schema.define(version: 20170303155206) do
 
 
@@ -42,5 +29,18 @@ ActiveRecord::Schema.define(version: 20170303155206) do
 
     t.index ["email"], name: "index_users_on_email", unique: true
   end
-
 end
+ActiveRecord::Schema.define(version: 20170303161306) do
+
+  create_table "microposts", force: :cascade do |t|
+    t.text     "content"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id", "created_at"], name: "index_microposts_on_user_id_and_created_at"
+    t.index ["user_id"], name: "index_microposts_on_user_id"
+  end
+end
+
+
+
