138c3d787cbb79e8a32a464c67d1f69c194b0067
Alex Donchev <a.donchev.12@aberdeen.ac.uk>
2017-03-03 19:30:59 +0000

    routes post /signup deleted

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,6 @@ Rails.application.routes.draw do
   get '/help', to: 'static_pages#help' 
   #/signup page is rooted the new action in users controller
   get  '/signup',  to: 'users#new'
-  post 'signup', to: 'users#create'
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
