5f196136702003abe11d6d704e2384a596384f7a
Alex Donchev <a.donchev.12@aberdeen.ac.uk>
2017-03-06 12:33:03 +0000

    picture fix

--- app/models/nasa_api.rb
+++ app/models/nasa_api.rb
@@ -2,8 +2,8 @@ class NasaApi < ApplicationRecord
 
 	def self.get_api_info
 		#We use self so we can use the method without creating an instance of the class everytime
-		nasa_api = "https://api.nasa.gov/planetary/apod?api_key=#{ENV['NASA_API_KEY']}"
-		puts nasa_api
+		nasa_api = "https://api.nasa.gov/planetary/apod?api_key=6urkm5VBJjqxIY3PVxTTGu0Bwub97CJ3iEP4ZY9A"
+		puts "###################" + nasa_api
 		request_to_nasa_api = Net::HTTP.get(URI(nasa_api))
 		JSON.parse request_to_nasa_api
 	end 
